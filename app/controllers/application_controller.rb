6ab6b55e6f3affc98a0cc8d67471f20b23f0a8e8
Carl Grafmuller <cgrafmuller@gmail.com>
2017-08-24 15:56:36 -0300

    redirect www

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :redirect_subdomain
+
+  def redirect_subdomain
+    return unless request.host == 'www.carlgrafmuller.com'
+    redirect_to 'https://carlgrafmuller.com' + request.fullpath, status: 301
+  end
 end
