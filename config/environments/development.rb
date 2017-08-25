5f1b631e5a9a444fb033968f6d28ca92e1a50da8
Carl Grafmuller <cgrafmuller@gmail.com>
2017-08-25 11:40:50 -0300

    back to 5.0.5

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,7 +18,7 @@ Rails.application.configure do
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      'Cache-Control' => "public, max-age=#{2.days.seconds.to_i}"
+      'Cache-Control' => 'public, max-age=172800'
     }
   else
     config.action_controller.perform_caching = false
@@ -51,7 +51,4 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-
-  # Load webpacker assets
-  config.x.webpacker[:dev_server_host] = "http://127.0.0.1:8080"
 end
