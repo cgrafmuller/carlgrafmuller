7e725d1c3bef9e90f581001099f343e056be1672
Carl Grafmuller <cgrafmuller@gmail.com>
2017-09-03 10:55:16 -0300

    page title & meta & layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@
 
 class PagesController < ApplicationController
   def index
+    @page_title = 'Carl Grafmuller'
+    @meta_description = 'Personal website of Carl Grafmuller. Entrepreneur, actuary, data scientist, full-stack developer, musician, artifical intelligence researcher.'
   end
 end
